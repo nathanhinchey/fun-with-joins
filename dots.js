8182ef14b451a370db13660c09a4e5415d34b8d9
Nathan Hinchey <nathanhinchey@gmail.com>
2016-10-26 12:34:12 -0400

    Rename 'boxes' to 'dots'

--- dots.js
+++ dots.js
@@ -1,30 +1,30 @@
 (function() {
   'use strict';
-  window.boxes = window.boxes || {};
+  window.dots = window.dots || {};
 
-  window.boxes.set1 = [
+  window.dots.set1 = [
     {x: 20, y:30, color:"blue"},
     {x: 40, y:20, color:"red"},
     {x: 10, y:50, color:"green"},
   ];
 
-  window.boxes.set2 = [
+  window.dots.set2 = [
     {x: 30, y:20, color:"blue"},
     {x: 50, y:30, color:"red"},
     {x: 5, y:80, color:"green"},
   ];
 
-  window.boxes.data = window.boxes.set1;
+  window.dots.data = window.dots.set1;
   var svg = d3.select("svg");
 
-  window.boxes.renderDots = function(){
+  window.dots.renderDots = function(){
     var circle = svg.selectAll("circle")
-      .data(window.boxes.data)
+      .data(window.dots.data)
 
     circle.exit().remove();
 
     circle.enter().append("circle")
-    // .merge(circle)
+    // .merge(circle) // for V4
     .attr("cx", function(d){ return d.x; })
     .attr("cy", function(d){ return d.y; })
     .attr("fill", function(d){ return d.color; })
